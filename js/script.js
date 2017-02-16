bc3edbd27acf334e019ec93e8c50dfc88c2afab1
eugenech1988 <eugenech1988@gmail.com>
2017-02-16 17:04:47 +0200

    constants

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
 window.onload = () => {
 
-    //declare variables
 
-    const Elements = {
-        phoneWrapper : document.getElementById('phone-wrapper'),
-        phoneVideo : document.getElementById('phone-video'),
-        rangeWrapp : document.getElementById('range-wrapper'),
-        rangeText : document.getElementById('range-notify'),
-        textWrapp : document.getElementById('text-wrapper'),
-        buttonWrapp : document.getElementById('buy-btn'),
-        loaderWrapp : document.getElementById('loader-wrapp'),
-        phoneVideoWrapper : document.getElementById('phone-video'),
-        inputRange : document.getElementById('range'),
-        phoneInternals : document.getElementById('internals'),
-        flashWrapp : document.getElementById('flash-wrapp'),
-        rainWrapp : document.getElementById('rain-wrapp'),
-        textArray : document.querySelectorAll('.text'),
-        closeWidgetIcon : document.getElementById('close-link'),
-        widgetWrapper : document.getElementById('outer-container')
-    };
 
     /**
      *
